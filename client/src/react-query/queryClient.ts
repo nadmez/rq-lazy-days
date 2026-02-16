61b1c17caa6c0ce383cc679f3bbb9b9757cbd350
nadmez <nadmez@yahoo.com>
2026-02-16 22:20:04 +0300

    Global query error handler

--- client/src/react-query/queryClient.ts
+++ client/src/react-query/queryClient.ts
@@ -1,21 +1,35 @@
 import { toast } from "@/components/app/toast";
 
-import { QueryClient } from "@tanstack/react-query";
+import { QueryCache, QueryClient } from "@tanstack/react-query";
 
-// function errorHandler(errorMsg: string) {
-//   // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
-//   // one message per page load, not one message per query
-//   // the user doesn't care that there were three failed queries on the staff page
-//   //    (staff, treatments, user)
-//   const id = "react-query-toast";
+function errorHandler(errorMsg: string) {
+  // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
+  // one message per page load, not one message per query
+  // the user doesn't care that there were three failed queries on the staff page
+  //    (staff, treatments, user)
+  const id = "react-query-toast";
 
-//   if (!toast.isActive(id)) {
-//     const action = "fetch";
-//     const title = `could not ${action} data: ${
-//       errorMsg ?? "error connecting to server"
-//     }`;
-//     toast({ id, title, status: "error", variant: "subtle", isClosable: true });
-//   }
-// }
+  if (!toast.isActive(id)) {
+    const action = "fetch";
+    const title = `could not ${action} data: ${
+      errorMsg ?? "error connecting to server"
+    }`;
+    toast({ id, title, status: "error", variant: "subtle", isClosable: true });
+  }
+}
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 600000, // 10 dakika (örnek)
+      gcTime: 900000, // 15 dakika (örnek) -- V4'te cacheTime idi
+      refetchOnWindowFocus: false,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: (error) => {
+      errorHandler(error.message);
+    },
+  }),
+});
