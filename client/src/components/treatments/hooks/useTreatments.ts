46adddcfac18dfcc36b6fd31f3969b6ab4bd11ba
nadmez <nadmez@yahoo.com>
2026-02-16 22:00:37 +0300

    Custom useTreatment hook

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,19 @@
 import type { Treatment } from "@shared/types";
+import { useQuery } from "@tanstack/react-query";
 
 import { axiosInstance } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get("/treatments");
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
-  return [];
+  const { data = [] } = useQuery({
+    queryFn: getTreatments,
+    queryKey: [queryKeys.treatments],
+  });
+  return data;
 }
